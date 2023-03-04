3fd25b356a0b3c70d723570d5648790a944148da
Luis Dourado <luis_dourado33@hotmail.com>
2023-03-04 15:09:46 -0400

    feat: add publicRouter navigation

--- src/root.tsx
+++ src/root.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
 import { ThemeProvider } from 'providers/theme';
+import { RouterProvider } from 'react-router-dom';
+
+import { publicRouter } from 'navigation/public';
 
 const Root = (): JSX.Element => {
 	return (
 		<ThemeProvider>
-			<h1>Hello World!</h1>
+			<RouterProvider router={publicRouter} />
 		</ThemeProvider>
 	);
 };
