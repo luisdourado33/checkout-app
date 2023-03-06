984f4af2c7443bc88f88efb5cd086faf3b25c4b0
Luis Dourado <luis_dourado33@hotmail.com>
2023-03-05 23:09:35 -0400

    feat: add spacing and breakpoints settings to theme

--- src/providers/theme/theme.config.ts
+++ src/providers/theme/theme.config.ts
@@ -17,4 +17,15 @@ export const theme: DefaultTheme = {
     normal: 300,
     bold: 500,
   },
+  spacing: {
+    inline: "100px",
+    block: "15px",
+  },
+  breakpoints: {
+    laptop: "1024px",
+    tablet: "768px",
+    mobileL: "425px",
+    mobileM: "375px",
+    mobileS: "320px",
+  },
 };
