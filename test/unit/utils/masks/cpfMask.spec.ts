24d4bc06781affd8c91065e9186dab1fb86ae566
Luis Dourado <luis_dourado33@hotmail.com>
2023-03-07 23:36:45 -0400

    test(mask): add cpfMask test suite

--- test/unit/utils/masks/cpfMask.spec.ts
+++ test/unit/utils/masks/cpfMask.spec.ts
@@ -1,23 +1,21 @@
 import { cpfMask } from "../../../../src/utils/masks";
 
-describe("Test suite for cpfMask function", () => {
-  let correctInput: string, incorrectInput: string, valueExpected: string;
+describe("Suite for cpfMask function", () => {
+  let correctInput: string, incorrectInput: string, expectedResult: string;
 
   beforeAll(() => {
     correctInput = "11111111111";
     incorrectInput = "111111111112";
-    valueExpected = "111.111.111-11";
+    expectedResult = "111.111.111-11";
   });
 
   it("should not be able to format current CPF (digits exceeded)", () => {
     const formattedCPF: string = cpfMask(incorrectInput);
-
-    expect(formattedCPF).not.toEqual(valueExpected);
+    expect(formattedCPF).not.toEqual(expectedResult);
   });
 
   it("should be able to format current CPF", () => {
     const formattedCPF = cpfMask(correctInput);
-
-    expect(formattedCPF).toEqual(valueExpected);
+    expect(formattedCPF).toEqual(expectedResult);
   });
 });
