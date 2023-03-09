675032c838836abae9b508f0c7a73ac618db0c8f
Luis Dourado <luis_dourado33@hotmail.com>
2023-03-08 23:50:39 -0400

    feat(redux): add application reducers

---
+++ src/store/reducers/authenticatedSlice.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { type ICheckoutForm } from "@types";
+import { type RootState } from "store/application-store";
+
+interface AuthenticatedState {
+  formFields: ICheckoutForm;
+  isFormFilled: boolean;
+}
+
+const initialState: AuthenticatedState = {
+  formFields: {
+    creditCardNumber: "",
+    cvv: "",
+    validityDate: "",
+    printedName: "",
+    cpf: "",
+    installments: 0,
+  },
+  isFormFilled: false,
+};
+
+export const authenticatedSlice = createSlice({
+  name: "authenticated",
+  initialState,
+  reducers: {
+    getFormFields: (state) => {},
+    setFormFields: (state, action: PayloadAction<ICheckoutForm>) => {
+      state.formFields = action.payload;
+    },
+    toggleIsFormFilled: (state) => {
+      state.isFormFilled = !state.isFormFilled;
+
+      if (!state.isFormFilled) {
+        state.formFields = initialState.formFields;
+      }
+    },
+  },
+});
+
+export const { getFormFields, setFormFields } = authenticatedSlice.actions;
+export const selectAuthenticated = (state: RootState) => state.authenticated;
+export default authenticatedSlice.reducer;
